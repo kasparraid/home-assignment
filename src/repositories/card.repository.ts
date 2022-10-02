f8459fd448b72ea51d621e4f3c7220b273f5863b
Kaspar Raid <kaspar.raidd@gmail.com>
2022-10-02 13:40:22 +0300

    docs: added explanation why createAll() is wrapped in transaction

--- src/repositories/card.repository.ts
+++ src/repositories/card.repository.ts
@@ -12,6 +12,10 @@ export class CardRepository extends DefaultTransactionalRepository<
     super(Card, dataSource);
   }
 
+  /**
+   * createAll() is wrapped in a transaction since regular createAll() does not guarantee to maintain order
+   * @param cards
+   */
   async createAllInTransaction(cards: Card[]) {
     const tx = await this.beginTransaction();
     const created = await this.createAll(cards, {transaction: tx})
